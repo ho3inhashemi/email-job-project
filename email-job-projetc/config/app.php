48a755fe74c2876df282ef956d7124f13e4edc45
ho3inhashemi <ho3inhashemi@gmail.com>
2022-02-06 20:35:31 +0330

    send email to user by job

--- email-job-projetc/config/app.php
+++ email-job-projetc/config/app.php
@@ -15,6 +15,9 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+
+    'email' => env('MAIL_FROM_ADDRESS'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
--- email-job-projetc/resources/views/emails/test.blade.php
+++ email-job-projetc/resources/views/emails/test.blade.php
@@ -11,7 +11,7 @@
 </h2>
 </center>
   
-<p>Hi, Sir</p>
+<p>Hi, Hossein</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
